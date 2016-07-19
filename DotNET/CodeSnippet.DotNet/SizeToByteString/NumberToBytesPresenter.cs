4aa6f7d3e000a195785f0f23d64dbca15fca367c
HarryPaek <paek.harry@gmail.com>
2016-07-19 17:00:31 +0900

    .net code snipet, Size to bytes string presentor

---
+++ DotNET/CodeSnippet.DotNet/SizeToByteString/NumberToBytesPresenter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeToByteString
+{
+    public class NumberToBytesPresenter
+    {
+        private string[] units = { "bytes", "KB", "MB", "GB", "TB", "PB" };
+
+        public NumberToBytesPresenter() : this(100)
+        {
+        }
+
+        public NumberToBytesPresenter (int sizeNumber)
+        {
+            this.SizeNumber = sizeNumber;
+        }
+
+        public int SizeNumber { get; set; }
+
+        public string GetSizeString(int precision)
+        {
+            if (precision < 1) precision = 1;
+
+            var unitIndex = (int)Math.Floor(Math.Log10(SizeNumber) / Math.Log10(1024));
+            string precisionFormat = string.Format("{{0:F{0}}} {{1}}", precision);
+
+            Console.WriteLine("Math.Log10(SizeNumber) = [{0}]", Math.Log10(SizeNumber));
+            Console.WriteLine("Math.Log10(1024) = [{0}]", Math.Log10(1024));
+            Console.WriteLine("unitIndex = [{0}]", unitIndex);
+            Console.WriteLine("precisionFormat = {0}", precisionFormat);
+
+            var result = string.Format(precisionFormat, SizeNumber / Math.Pow(1024, unitIndex), units[unitIndex]);
+
+            return result;
+        }
+    }
+}
---
+++ DotNET/CodeSnippet.DotNet/SizeToByteString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeToByteString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presenter = new NumberToBytesPresenter();
+
+            presenter.SizeNumber = 1000000;
+            Console.WriteLine("{0} = [{1}]", presenter.SizeNumber, presenter.GetSizeString(2));
+            Console.WriteLine("===============================================================================");
+
+            presenter.SizeNumber = 1100000;
+            Console.WriteLine("{0} = [{1}]", presenter.SizeNumber, presenter.GetSizeString(2));
+            Console.WriteLine("===============================================================================");
+
+            presenter.SizeNumber = 1111100000;
+            Console.WriteLine("{0} = [{1}]", presenter.SizeNumber, presenter.GetSizeString(2));
+            Console.WriteLine("===============================================================================");
+
+            Console.ReadLine();
+        }
+    }
+}
