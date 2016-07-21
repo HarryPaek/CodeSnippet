0ad0341c3d7082951c08f9e0beed82903791d500
HarryPaek <paek.harry@gmail.com>
2016-07-21 18:03:52 +0900

    Test Code for Event Hooking added

---
+++ DotNET/CodeSnippet.DotNet/EventHooking.Business/LifecycleService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHooking.Business
+{
+    public class LifecycleService
+    {
+        public Status GetNextLifecycleStatus()
+        {
+
+            return Status.InWork;
+        }
+    }
+
+    public enum Status
+    {
+        Planned,
+        InWork,
+        InReview,
+        Approved,
+        Obsoleted
+    }
+}
---
+++ DotNET/CodeSnippet.DotNet/EventHooking.Business/WorkflowService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHooking.Business
+{
+    public class WorkflowService
+    {
+        public void CompleteActivity(int activityId)
+        {
+
+        }
+    }
+}
