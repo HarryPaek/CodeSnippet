c55cf6232e78ae91816a7de2b3eaf5bbd11ad377
HarryPaek <paek.harry@gmail.com>
2016-07-22 09:50:06 +0900

    Custom Event Sample Code Added

---
+++ DotNET/CodeSnippet.DotNet/CustomEventExample/Counter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Counter
+    {
+        private int threshold;
+        private int total;
+
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+
+        public Counter(int threshold)
+        {
+            this.threshold = threshold;
+        }
+
+        public void Add(int x)
+        {
+            total += x;
+
+            if(total >= threshold)
+            {
+                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs {
+                    Threshold = threshold,
+                    TimeReached = DateTime.Now
+                };
+
+                OnThresholdReached(args);
+            }
+        }
+
+        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+        {
+            EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
+
+            if (handler != null)
+                handler(this, e);
+        }
+    }
+}
---
+++ DotNET/CodeSnippet.DotNet/CustomEventExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter(new Random().Next(10, 20));
+            counter.ThresholdReached += counter_ThresholdReached1;
+            counter.ThresholdReached += counter_ThresholdReached2;
+
+            Console.WriteLine("press 'a' key to increase total");
+            
+            while (Console.ReadKey(true).KeyChar == 'a')
+            {
+                Console.WriteLine("adding one");
+                counter.Add(1);
+            }
+
+
+            Console.ReadLine();
+        }
+
+
+        static void counter_ThresholdReached1(object sender, ThresholdReachedEventArgs e)
+        {
+            Console.WriteLine("##### 00001 The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
+        }
+
+        static void counter_ThresholdReached2(object sender, ThresholdReachedEventArgs e)
+        {
+            Console.WriteLine("##### 00002 The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
+        }
+    }
+}
