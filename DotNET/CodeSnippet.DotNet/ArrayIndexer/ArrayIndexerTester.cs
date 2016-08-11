6648fccd0cc380f36812f2005e6159d7b4834af9
HarryPaek <paek.harry@gmail.com>
2016-08-11 11:11:46 +0900

    Array Indexer Test with C#6.0 which is not working

---
+++ DotNET/CodeSnippet.DotNet/ArrayIndexer/ArrayIndexerTester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayIndexer
+{
+    public class ArrayIndexerTester
+    {
+        private readonly int[,] _table;
+
+        public ArrayIndexerTester() : this(5, 5)
+        {
+        }
+
+        public ArrayIndexerTester(int sizeX, int sizeY)
+        {
+            _table = new int[5, 5];
+        }
+
+        public int this[int idxX, int idxY]
+        {
+            get
+            {
+                return _table[idxX, idxY];
+            }
+            set
+            {
+                _table[idxX, idxY] = value;
+            }
+        }
+
+        // public int this[int idxX, int idxY] => _table[idxX, idxY];  /* C# 6.0 Only? */
+    }
+}
---
+++ DotNET/CodeSnippet.DotNet/ArrayIndexer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var indexer = new ArrayIndexerTester();
+            
+            indexer[0, 0] = 10;
+            indexer[0, 1] = 20;
+            indexer[0, 2] = 30;
+            indexer[0, 3] = 40;
+            indexer[0, 4] = 50;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("indexer[0, {0}] = [{1}]", i, indexer[0, i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
