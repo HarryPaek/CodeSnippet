934e990cde287456d57eb2c0f9ab9051d544bdb8
HarryPaek <paek.harry@gmail.com>
2016-08-19 18:43:09 +0900

    XElement Examples Added

--- DotNET/CodeSnippet.DotNet/EventHooking.Business/WorkflowService.cs
+++ DotNET/CodeSnippet.DotNet/EventHooking.Business/WorkflowService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace EventHooking.Business
 {
@@ -10,7 +11,6 @@ namespace EventHooking.Business
     {
         public void CompleteActivity(int activityId)
         {
-
         }
     }
 }
---
+++ DotNET/CodeSnippet.DotNet/XElementExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XElementExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement root = new XElement("Record",
+                                         new XAttribute("Epoc", 2000),
+                                         new XElement("Name",
+                                                      new XElement("First", "Mike"),
+                                                      new XElement("Second")),
+                                         new XElement("Address"));
+
+            Console.WriteLine("Xml Text = [{0}]", root.ToString(SaveOptions.DisableFormatting));
+
+            XDocument xDoc = new XDocument(root);
+            Console.WriteLine("xDoc Text = [{0}]", xDoc.ToString(SaveOptions.DisableFormatting));
+
+            Console.ReadLine();
+        }
+    }
+}
