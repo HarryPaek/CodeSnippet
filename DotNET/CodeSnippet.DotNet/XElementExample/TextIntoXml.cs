20c62fdbb1fd8f1eb40a8f95775ad36702e64127
HarryPaek <paek.harry@gmail.com>
2016-08-22 11:08:48 +0900

    textIntoXml Updated

--- DotNET/CodeSnippet.DotNet/XElementExample/TextIntoXml.cs
+++ DotNET/CodeSnippet.DotNet/XElementExample/TextIntoXml.cs
@@ -11,8 +11,25 @@ namespace XElementExample
     {
         public XDocument GetXmlDocument()
         {
-            string sampleXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><activity><input/><work/><user><currentuser name=\"[TestUser02]Test User 02\" key=\"TestUser02\" table=\"USER\" title=\"[TestUser02]Test User 02\"/><currentuser name=\"[TestUser01]Test User 01\" key=\"TestUser01\" table=\"USER\" title=\"[TestUser01]Test User 01\"/></user><map/></activity>";
-            XDocument xmlDocument = XDocument.Parse(sampleXml);
+            StringBuilder sampleXmlBuider = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            sampleXmlBuider.Append("<activity>");
+            sampleXmlBuider.Append("<input>");
+            sampleXmlBuider.Append("<inputwork name=\"Design Review.xls\" key=\"27\" table=\"DOCUMENT_TEMPLATE\" title=\"Design review\" />");
+            sampleXmlBuider.Append("<inputwork name=\"Design FMEA.xls\" key=\"29\" table=\"DOCUMENT_TEMPLATE\" title=\"Design FMEA\" />");
+            sampleXmlBuider.Append("</input>");
+            sampleXmlBuider.Append("<work>");
+            sampleXmlBuider.Append("<currentwork name=\"BOMLIST.xlsx\" key=\"538\" table=\"DOCUMENT\" title=\"BOMLIST\" />");
+            sampleXmlBuider.Append("<currentwork name=\"PDM 서버.txt\" key=\"532\" table=\"DOCUMENT\" title=\"테스트1111\" />");
+            sampleXmlBuider.Append("<currentwork name=\"Test Report.xls\" key=\"26\" table=\"DOCUMENT_TEMPLATE\" title=\"Test Report\" />");
+            sampleXmlBuider.Append("<currentwork name=\"BOM of Cup.txt\" key=\"31\" table=\"DOCUMENT_TEMPLATE\" title=\"Kim 0329\" />");
+            sampleXmlBuider.Append("</work>");
+            sampleXmlBuider.Append("<user>");
+            sampleXmlBuider.Append("<currentuser name=\"[TestUser02]Test User 02\" key=\"TestUser02\" table=\"USER\" title=\"[TestUser02]Test User 02\"/>");
+            sampleXmlBuider.Append("<currentuser name=\"[TestUser01]Test User 01\" key=\"TestUser01\" table=\"USER\" title=\"[TestUser01]Test User 01\"/>");
+            sampleXmlBuider.Append("</user>");
+            sampleXmlBuider.Append("<map />");
+            sampleXmlBuider.Append("</activity>");
+            XDocument xmlDocument = XDocument.Parse(sampleXmlBuider.ToString());
 
             return xmlDocument;
         }
