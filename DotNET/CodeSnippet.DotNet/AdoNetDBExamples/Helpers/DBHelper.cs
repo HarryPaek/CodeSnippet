a026b6b7a53a4c1178dc58f7f02a6b43b50b4e61
HarryPaek <paek.harry@gmail.com>
2016-08-24 19:04:31 +0900

    DbDataReader extension method SafeGetValue<T> added

---
+++ DotNET/CodeSnippet.DotNet/AdoNetDBExamples/Helpers/DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDBExamples.Helpers
+{
+    public static class DBHelper
+    {
+        public static T? SafeGetValue<T>(this DbDataReader reader, int colIndex) where T : struct
+        {
+            if (!reader.IsDBNull(colIndex))
+                return (T)reader.GetValue(colIndex);
+
+            return null;
+        }
+    }
+}
