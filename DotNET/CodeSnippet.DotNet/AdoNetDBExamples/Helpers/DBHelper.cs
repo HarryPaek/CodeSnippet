a7cbc0286b5e1538c052c91da3958d9973902ce9
HarryPaek <paek.harry@gmail.com>
2016-08-24 19:47:15 +0900

    DbDataReader extension added

--- DotNET/CodeSnippet.DotNet/AdoNetDBExamples/Helpers/DBHelper.cs
+++ DotNET/CodeSnippet.DotNet/AdoNetDBExamples/Helpers/DBHelper.cs
@@ -12,7 +12,24 @@ namespace AdoNetDBExamples.Helpers
         public static T? SafeGetValue<T>(this DbDataReader reader, int colIndex) where T : struct
         {
             if (!reader.IsDBNull(colIndex))
-                return (T)reader.GetValue(colIndex);
+            {
+                var value = reader.GetValue(colIndex);
+
+                if (value is T)
+                    return (T)value;
+                else
+                {
+                    return (T)Convert.ChangeType(value, typeof(T));
+                }
+            }
+
+            return null;
+        }
+
+        public static string SafeGetString(this DbDataReader reader, int colIndex)
+        {
+            if (!reader.IsDBNull(colIndex))
+                return reader.GetString(colIndex);
 
             return null;
         }
