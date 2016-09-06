9e646a6c2f1069b16f3babe92e07ba73b6dfd5ae
HarryPaek <paek.harry@gmail.com>
2016-09-06 23:46:49 +0900

    C# Algorithm Quiz added

---
+++ CSharp/CSharp.CodeSnippet/BackToAlgorithms/Flog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackToAlgorithms
+{
+    using System;
+
+    public class Frog
+    {
+        public static int NumberOfWays(int n)
+        {
+            int maxStep = n;
+            int minStep = n/2 + n%2;
+            int totalCase = maxStep - minStep;
+            int totalWay = 0;
+          
+        
+            for(int i = 0; i<= totalCase; i++) {
+                totalWay += fac(minStep + i) / (fac(minStep - i) * fac(i));
+            }
+
+            return totalWay;
+        }
+
+        private static int fac(int num)
+        {
+            if (num < 2)
+                return 1;
+
+            return num * fac(num - 1);
+        }
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(NumberOfWays(3));
+        }
+    }
+}
