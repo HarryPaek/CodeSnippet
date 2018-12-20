36f7fe78c740262bf292aee6740894a617a16b60
Harry Paek <paek.harry@gmail.com>
2018-12-20 09:24:00 +0900

    more codes added

---
+++ CSharp/CSharp.CodeTech.Basic/Ch111.ExtendedClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ch111.ExtendedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Base> baseList = new List<Base>(new Base[] { new Base(), new Extended() });
+
+            foreach (var item in baseList.OfType<Extended>())
+            {
+                item.SayIt();
+            }
+        }
+    }
+
+    internal class Extended : Base
+    {
+        public void SayIt()
+        {
+            Console.WriteLine("I am Extended!");
+        }
+    }
+
+    internal class Base
+    {
+    }
+}
---
+++ CSharp/CSharp.CodeTech.Basic/Ch112.StructMeaningless/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ch112.StructMeaningless
+{
+    struct TenMumbersStruct
+    {
+        public double t00, t01, t02, t03, t04, t05, t06, t07, t08, t09;
+    }
+
+    class TenMumbersClass
+    {
+        public double t00, t01, t02, t03, t04, t05, t06, t07, t08, t09;
+    }
+
+    class Program
+    {
+        private static double calcStruct(TenMumbersStruct t)
+        {
+            return t.t00 + t.t01 + t.t02 + t.t03 + t.t04 + t.t05 + t.t06 + t.t07 + t.t08 + t.t09;
+        }
+
+        private static double calcClass(TenMumbersClass t)
+        {
+            return t.t00 + t.t01 + t.t02 + t.t03 + t.t04 + t.t05 + t.t06 + t.t07 + t.t08 + t.t09;
+        }
+
+        static void Main(string[] args)
+        {
+            double sumStruct = 0;
+            double sumClass = 0;
+
+            var start = DateTime.Now;
+
+            for (int index = 0; index < 100000000; index++)
+            {
+                sumStruct += calcStruct(new TenMumbersStruct());
+            }
+
+            Console.WriteLine("Struct Sum = [{0}]", sumStruct);
+            Console.WriteLine("Struct Elapsed Time = [{0}]", DateTime.Now - start);
+
+            start = DateTime.Now;
+
+            for (int index = 0; index < 100000000; index++)
+            {
+                sumClass += calcClass(new TenMumbersClass());
+            }
+
+            Console.WriteLine("Class Sum = [{0}]", sumClass);
+            Console.WriteLine("Class Elapsed Time = [{0}]", DateTime.Now - start);
+
+            Console.ReadLine();
+        }
+    }
+}
