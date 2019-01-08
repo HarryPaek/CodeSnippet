3bcf98c26554d05b9678e9d29debaa361544009b
Harry Paek <paek.harry@gmail.com>
2019-01-08 16:14:49 +0900

    Thread Lock Sample Added

---
+++ CSharp/CSharp.CodeSnippet/ThreadLockSample/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThreadLockSample
+{
+    public class Account
+    {
+        private readonly object _balanceLock = new object();
+        private decimal _balance;
+
+        public Account(decimal initialBalance)
+        {
+            this._balance = initialBalance;
+        }
+
+        public decimal Debit(decimal amount, int index)
+        {
+            lock (this._balanceLock) {
+                if (this._balance >= amount) {
+                    Console.WriteLine("Task [{0, 10}], Balance before debit: {1, 5}", index, this._balance);
+                    Console.WriteLine("Task [{0, 10}], Amount to remove    : {1, 5}", index, amount);
+                    this._balance = this._balance - amount;
+                    Console.WriteLine("Task [{0, 10}], Balance after debit : {1, 5}", index, this._balance);
+
+                    return amount;
+                }
+                else {
+                    return 0;
+                }
+            }
+        }
+
+        public void Credit(decimal amount, int index)
+        {
+            lock (this._balanceLock) {
+                Console.WriteLine("Task [{0, 10}], Balance before credit: {1, 5}", index, this._balance);
+                Console.WriteLine("Task [{0, 10}], Amount to add        : {1, 5}", index, amount);
+                this._balance = this._balance + amount;
+                Console.WriteLine("Task [{0, 10}], Balance after credit : {1, 5}", index, this._balance);
+            }
+        }
+
+        public decimal Balance(int index)
+        {
+            lock (this._balanceLock) {
+                Console.WriteLine("Task [{0, 10}], Current Balance      : {1, 5}", index, this._balance);
+                return this._balance;
+            }
+        }
+    }
+}
---
+++ CSharp/CSharp.CodeSnippet/ThreadLockSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadLockSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account(1000);
+            var tasks   = new Task[100];
+
+            var start = DateTime.Now;
+
+            for (int index = 0; index < tasks.Length; index++)
+            {
+                int taskIndex = index;
+                tasks[index] = Task.Run(() => RandomUpdate(account, taskIndex));
+            }
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Tasks were done!!!, Total Time: [{0}]", DateTime.Now - start);
+            Console.ReadLine();
+        }
+
+        private static void RandomUpdate(Account account, int index)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                var amount = rnd.Next(1, 100);
+                double nextDouble = rnd.NextDouble();
+                bool doCredit  = nextDouble < 0.35;
+                bool doBalance = nextDouble < 0.7;
+
+                if (doCredit)
+                    account.Credit(amount, index);
+                else if (doBalance)
+                    account.Balance(index);
+                else
+                    account.Debit(amount, index);
+            }
+        }
+    }
+}
