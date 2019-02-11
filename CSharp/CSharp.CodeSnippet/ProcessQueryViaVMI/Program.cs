ce594220b27681ca51490a4e3f985e01865d4d82
Harry Paek <paek.harry@gmail.com>
2019-02-11 16:37:37 +0900

    Docker vs. VMWare Switch Command

---
+++ CSharp/CSharp.CodeSnippet/ProcessQueryViaVMI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessQueryViaVMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wmiQueryString = "SELECT * FROM Win32_Process";
+            using (var searcher = new ManagementObjectSearcher(wmiQueryString))
+            using (var results = searcher.Get())
+            {
+                int index = -1;
+                foreach (var result in results)
+                {
+                    index++;
+                    ManagementObject mo = result as ManagementObject;
+
+                    if(mo == null)
+                        continue;
+
+                    Console.WriteLine("ExecutablePath[{0}] = [{1}], CommandLine[{0}] = [{2}]", index, mo["ExecutablePath"], mo["CommandLine"]);
+                }
+
+                /*
+                var query = from p in Process.GetProcesses()
+                            join mo in results.Cast<ManagementObject>()
+                            on p.Id equals (int)(uint)mo["ProcessId"]
+                            select new
+                            {
+                                Process = p,
+                                Path = (string)mo["ExecutablePath"],
+                                CommandLine = (string)mo["CommandLine"],
+                            };
+                foreach (var item in query)
+                {
+                    // Do what you want with the Process, Path, and CommandLine
+                }
+                */
+            }
+
+            Console.WriteLine("Done!!!");
+            Console.ReadLine();
+        }
+    }
+}
