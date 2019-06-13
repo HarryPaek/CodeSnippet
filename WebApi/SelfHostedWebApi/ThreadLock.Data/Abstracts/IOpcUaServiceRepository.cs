fefac7962d4032f0af27c6db5fa0b77e0a9dcf95
Harry Paek <paek.harry@gmail.com>
2019-06-13 13:44:43 +0900

    OWIN Server Added  - OpcUaService Test Service Added for SMIC Test

---
+++ WebApi/SelfHostedWebApi/ThreadLock.Data/Abstracts/IOpcUaServiceRepository.cs
@@ -0,0 +1,9 @@
+﻿using ePlatform.Data.Abstracts;
+using ThreadLock.Data.Models;
+
+namespace ThreadLock.Data.Abstracts
+{
+    public interface IOpcUaServiceRepository : IRepository<string, OpcUaServiceRequest>
+    {
+    }
+}
