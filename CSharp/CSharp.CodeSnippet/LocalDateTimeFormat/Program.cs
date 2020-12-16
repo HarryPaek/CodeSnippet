b110c292d7a4831b01b7df2182cfbaed016b42c8
Harry Paek <paek.harry@gmail.com>
2020-12-16 19:19:53 +0900

    LocalDateTimeFormat Project Added

---
+++ CSharp/CSharp.CodeSnippet/LocalDateTimeFormat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace LocalDateTimeFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetUICulture();
+
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("Now = [{0}]", now.ToString());
+
+            Console.ReadLine();
+        }
+
+        private static void SetUICulture()
+        {
+            try
+            {
+                // var newCulture = new CultureInfo("en-GB");
+                // var newCulture = new CultureInfo("en-GB");
+                var newCulture = new CultureInfo("en-US");
+
+                //Culture for any thread
+                CultureInfo.DefaultThreadCurrentCulture = newCulture;
+
+                //Culture for UI in any thread
+                CultureInfo.DefaultThreadCurrentUICulture = newCulture;
+            }
+            catch
+            {
+            }
+        }
+
+    }
+}
