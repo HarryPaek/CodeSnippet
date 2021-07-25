63c7d4710c1d9c34e1cc2516ea917894b5a54bd7
Harry Paek <paek.harry@gmail.com>
2021-07-26 08:00:06 +0900

    Unicode Escape sample code added

---
+++ CSharp/CSharp.CodeSnippet/UnicodeToAsciiEscape/Extensions/UnicodeStringExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UnicodeToAsciiEscape.Extensions
+{
+    public static class UnicodeStringExtension
+    {
+        public static string EncodeNonAsciiChars(this string unicodeText)
+        {
+            if (string.IsNullOrWhiteSpace(unicodeText))
+                return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in unicodeText)
+            {
+                sb.AppendFormat("\\u{0}", ((int)c).ToString("x4"));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string DecodeNonAsciiChars(this string asciiText)
+        {
+            if (string.IsNullOrWhiteSpace(asciiText))
+                return string.Empty;
+
+            return Regex.Replace(asciiText, @"\\u(?<Value>[a-zA-Z0-9]{4})", m => { return ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString(); });
+        }
+    }
+}
---
+++ CSharp/CSharp.CodeSnippet/UnicodeToAsciiEscape/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnicodeToAsciiEscape.Extensions;
+
+namespace UnicodeToAsciiEscape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unicodeString = @"백종근\AA\uabc1\u백"; // This string contains the unicode character Pi (\u03a0)";
+            string escapedText = unicodeString.EncodeNonAsciiChars();
+            string newUnicodeString = escapedText.DecodeNonAsciiChars();
+
+            Console.WriteLine("Original string: {0}", unicodeString);
+            Console.WriteLine("escapedText string: {0}", escapedText);
+            Console.WriteLine("New Unicode string: {0}", newUnicodeString);
+
+            /*
+            // Create two different encodings.
+            Encoding ascii = Encoding.ASCII;
+            Encoding unicode = Encoding.Unicode;
+
+            // Convert the string into a byte array.
+            byte[] unicodeBytes = unicode.GetBytes(unicodeString);
+
+            // Perform the conversion from one encoding to the other.
+            byte[] asciiBytes = Encoding.Convert(unicode, ascii, unicodeBytes);
+
+            // Convert the new byte[] into a char[] and then into a string.
+            char[] asciiChars = new char[ascii.GetCharCount(asciiBytes, 0, asciiBytes.Length)];
+            ascii.GetChars(asciiBytes, 0, asciiBytes.Length, asciiChars, 0);
+            string asciiString = new string(asciiChars);
+
+            // Display the strings created before and after the conversion.
+            Console.WriteLine("Original string: {0}", unicodeString);
+            Console.WriteLine("Ascii converted string: {0}", asciiString);
+
+            Console.ReadLine();
+
+            asciiBytes = ascii.GetBytes(asciiString);
+            unicodeBytes = Encoding.Convert(ascii, unicode, asciiBytes);
+
+            char[] unicodeChars = new char[unicode.GetCharCount(unicodeBytes, 0, unicodeBytes.Length)];
+            unicode.GetChars(unicodeBytes, 0, unicodeBytes.Length, unicodeChars, 0);
+
+            string newUnicodeString = new string(unicodeChars);
+
+            // Display the strings created before and after the conversion.
+            Console.WriteLine("Original string: {0}", unicodeString);
+            Console.WriteLine("New Unicode string: {0}", newUnicodeString);
+            */
+
+            Console.ReadLine();
+        }
+    }
+}
